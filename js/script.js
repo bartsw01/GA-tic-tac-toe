db6606adc50b3a83016f95ce12d944c99dda32c1
Bart Webber <bartacus.webber@gmail.com>
2020-04-05 21:00:37 -0700

    create gameboard html js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 console.log("I'm working");
+
+let emptyGameBoard = ["", "", "", "", "", "", "", "", ""];
+const humanPlayer = 'X';
+const computerPlayer = 'O';
+const winningCombos = [
+    [0, 1, 2], // across top row
+    [3, 4, 5], // across middle row
+    [6, 7, 8], // across bottom row
+    [0, 3, 6], // top left to bottom left
+    [1, 4, 7], // top middle to bottom middle
+    [2, 5, 8], // right top to right bottom
+    [0, 4, 8], // diagonal top left to bottom right
+    [6, 4, 2]  // diagonal top right to bottom left
+];
+
+const squares = document.querySelectorAll('.square');
